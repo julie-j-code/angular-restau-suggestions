0168aa18cd5beee8e1df871c8cf852ddabfd3230
Julie J <jeannet.julie@gmail.com>
2022-11-25 08:34:41 +0100

    Reactivation Collection Firestore Database et ajustements CSS

--- my-firestore.ts
+++ my-firestore.ts
@@ -1,10 +1,21 @@
-var firebaseConfig = {
-  apiKey: "AIzaSyARTpOZ7T6LijxfPWKvuO4-SWSoHKtmVzQ",
-  authDomain: "where-to-eat-11dae.firebaseapp.com",
-  projectId: "where-to-eat-11dae",
-  storageBucket: "where-to-eat-11dae.appspot.com",
-  messagingSenderId: "655331191691",
-  appId: "1:655331191691:web:0915ae4bf47c1a6f625d8e"
+// c'est juste une bonne pratique
+
+// var firebaseConfig = {
+//   apiKey: "AIzaSyARTpOZ7T6LijxfPWKvuO4-SWSoHKtmVzQ",
+//   authDomain: "where-to-eat-11dae.firebaseapp.com",
+//   projectId: "where-to-eat-11dae",
+//   storageBucket: "where-to-eat-11dae.appspot.com",
+//   messagingSenderId: "655331191691",
+//   appId: "1:655331191691:web:0915ae4bf47c1a6f625d8e"
+// };
+const firebaseConfig = {
+  apiKey: "AIzaSyCzaPgwU8DjjqTAnZt9NJx6PmpKlpP8uSI",
+  authDomain: "ng-where-to-eat-79f4f.firebaseapp.com",
+  databaseURL: "https://ng-where-to-eat-79f4f-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "ng-where-to-eat-79f4f",
+  storageBucket: "ng-where-to-eat-79f4f.appspot.com",
+  messagingSenderId: "516989252541",
+  appId: "1:516989252541:web:def85ac4b32d466c0058d4"
 };
 
 export default firebaseConfig;
